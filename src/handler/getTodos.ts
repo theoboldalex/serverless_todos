292e805c4325ba7a4434fe9e28ca44f05712da79
theoboldalex <theoboldalex@gmail.com>
2022-05-29 21:50:27 +0100

    Create home labmda for all todos with mock data

---
+++ src/handler/getTodos.ts
@@ -0,0 +1,29 @@
+"use strict";
+
+interface Todo {
+    id: number,
+    item: string
+}
+
+const mockTodos: Array<Todo> = [
+    {id: 1, item: 'wash car'},
+    {id: 2, item: 'write lambda'}
+]
+
+const getTodos = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: mockTodos,
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports = {
+    handler: getTodos
+}
